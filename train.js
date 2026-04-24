61a75cbc7f1eb2d4864d93ef831e5e3708a3fb4f
muza <itsmuuza@icloud.com>
2026-04-24 17:35:49 +0900

    Comment out async function and examples in train.js
    Comment out asynchronous function examples and related code.

--- train.js
+++ train.js
@@ -59,3 +59,50 @@ maslahatBering(70, (err, data) => {
 });
 console.log("bottom")
 */
+
+
+// Asynchornous functions
+// console.log("Jack Ma maslahatlari:");
+// const list = [
+//     'yaxshi talaba boling',
+//     'togri boshliq tanlang va koprot hato qiling',
+//     'uzingiga ishlashni boshlang',
+//     'siz kuchli bolgan narsalarni qiling',
+//     'yoshlarga investitsiya qiling',
+//     'endi dam oling'
+// ];
+
+// async function maslahatBering(age) {
+//     if(typeof age !== 'number') throw new Error("insert a number");
+//     else if (age <= 20) return list[0];
+//     else if (age < 30) return list[1];
+//     else if (age < 40) return list[2];
+//     else if (age < 50) return list [3];
+//     else if (age < 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(list[5])
+//             }, 2000);
+//         })
+//     }
+// }
+
+
+// then/catch
+// console.log("top")
+// maslahatBering(55).then(data => console.log("javob:", data))
+// .catch(err => console.log("ERROR:", err));
+// console.log("bottom")
+
+
+// async/await
+// async function run() {
+//     let javob = await maslahatBering(20);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering('asdf');
+//     console.log(javob);
+// }
+// run();
