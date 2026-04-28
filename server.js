5fadcc458589ebf22c1a1fb37a44eb2f0dd957ba
Muzaffar <itsmuuza@icloud.com>
2026-04-28 17:23:13 +0900

    IP 0.0.0.0/0 added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 const mongodb = require('mongodb')
 
 
-const connectionString = 'mongodb+srv://muza:apple.One1@cluster0.sk6h5i1.mongodb.net/Plan'
+const connectionString = 'mongodb+srv://muza:apple.One1@cluster0.zeflua5.mongodb.net/Plan'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
     if(err) console.log("ERROR on connection MongoDB")
     else {
